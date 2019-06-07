b4b07f5e8ff4e451d3a6dd1626d875c800e9ce84
Juan Ignacio Ubeira <jubeira@ekumenlabs.com>
2019-06-07 12:24:43 -0300

    Separating source containers in base and top.

--- ros2/dashing/docker_src/dashing_base/build.sh
+++ ros2/dashing/docker_src/dashing_base/build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-IMAGE=ros2_dashing_bionic_source
+IMAGE=ros2_dashing_bionic_base
 
 pushd "$( dirname "${BASH_SOURCE[0]}" )"
-./dashing_base/build.sh
 docker build -t ${IMAGE} . $@
 popd
---
+++ ros2/dashing/docker_src/dashing_base/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+REPO_ROOT=$SCRIPTPATH/../
+IMAGE=ros2_dashing_bionic_source
+
+if [ "$#" == 0 ]; then
+  EXTRA_ARGS="bash"
+else
+  EXTRA_ARGS="bash -c \"$@\""
+fi
+
+xhost +
+docker run \
+  --net=host -e DISPLAY=${DISPLAY} -v /dev:/dev \
+  -v ${HOME}:${HOME} \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  --privileged \
+  -it \
+  ${IMAGE} "${EXTRA_ARGS}"
